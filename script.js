d842dbfcfe6c6e52d3ab8e751bb62e60fc167ac2
Martinez Colomina <raul.martinez-colomina-external@capgemini.com>
2021-05-20 14:07:31 +0200

    v2.1

--- script.js
+++ script.js
@@ -12,14 +12,13 @@ fetch('https://pokeapi.co/api/v2/pokemon/')
  function printPokemons(pokemons){
      for(let i = 0; i < pokemons.length; i++){
         let pok = "pokemon" + i 
-        let img = "img" + i
-        let num = "numero" + i
+
 
         fetch(pokemons[i].url)
         .then(res =>  res.json())
         .then(json => { json
             
-           console.log(json)
+           //console.log(json)
            var list = document.getElementsByClassName(pok)[0]
            
            list.getElementsByClassName("img")[0].src = json.sprites.other.dream_world.front_default
@@ -30,7 +29,18 @@ fetch('https://pokeapi.co/api/v2/pokemon/')
            var bocadillo = list.getElementsByClassName("bocadillo")[0]
            bocadillo.getElementsByClassName("peso")[0].innerHTML = "Weight: "+ json.weight
            bocadillo.getElementsByClassName("altura")[0].innerHTML = "Height: "+ json.height
-           bocadillo.getElementsByClassName("experiencia")[0].innerHTML = "Ecperience: "+ json.base_experience
+           bocadillo.getElementsByClassName("experiencia")[0].innerHTML = "Experience: "+ json.base_experience
+
+           var abilities = list.getElementsByClassName("abilities")[0]
+           console.log(abilities.getElementsByClassName("lista")[0].getElementsByClassName("primero")[0])
+           for (let j = 0; j < json.abilities.length; j++) {
+            var avility = abilities.getElementsByClassName("lista")[0]
+            avility.getElementsByClassName(j)[0].innerHTML = json.abilities[j].ability.name
+
+            
+           }
+               
+
         })
      }
  }   
